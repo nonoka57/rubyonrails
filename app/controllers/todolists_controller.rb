fb68819712f22d95d538ebad7de551950aeb9258
EC2 Default User <ec2-user@ip-172-31-18-211.ec2.internal>
2021-10-04 01:38:04 +0000

    [fix]ルートのスペルミス修正

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,7 +3,7 @@ class TodolistsController < ApplicationController
     #  viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
     @list = List.new
   end
-  
+
   # 以下を追加
   def create
     # 1. データを新規登録するためのインスタンス作成
@@ -13,30 +13,30 @@ class TodolistsController < ApplicationController
     # 詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
-  
+
   def index
     @lists=List.all
   end
-  
+
   def show
     @list=List.find(params[:id])
   end
-  
+
   def edit
     @list=List.find(params[:id])
   end
-  
+
   def update
     list=List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
 
-  
+
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:gititle,:body)
+    params.require(:list).permit(:title,:body)
   end
-  
+
 end
